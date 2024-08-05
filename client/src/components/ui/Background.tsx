ae302733593c9444bd9a9913830c922a1e0c96e3
4mbareesh <57589799+4mbareesh@users.noreply.github.com>
2024-08-05 17:15:22 +0530

    fix: change link icon

--- client/src/components/ui/Background.tsx
+++ client/src/components/ui/Background.tsx
@@ -3,17 +3,19 @@ import React from "react";
 import { BackgroundBeams } from "./background-beams";
 import { cn } from "@/utils/cn";
 
-
 type BackgroundProps = {
   children: React.ReactNode;
   ClassName: string;
 };
 
-export function Background({ children, ClassName }: BackgroundProps): JSX.Element {
+export function Background({
+  children,
+  ClassName,
+}: BackgroundProps): JSX.Element {
   return (
     <div
       className={cn(
-        "h-fit w-full xl:px-32 xl:pt-44 px-10 md:px-20 md:pt-32 pt-20 bg-black flex flex-col items-center justify-center antialiased",
+        "flex h-fit w-full flex-col items-center justify-center bg-black px-10 pt-20 antialiased md:px-20 md:pt-32 lg:pt-72 xl:px-32 xl:pt-44 2xl:pt-20",
         ClassName
       )}
     >

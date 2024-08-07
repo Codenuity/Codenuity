f79d571be9a1b87d42afc39a8963e4054ab26639
4mbareesh <57589799+4mbareesh@users.noreply.github.com>
2024-08-08 00:06:03 +0530

    chore: change content in not-found page

--- client/src/components/not-found/Content.tsx
+++ client/src/components/not-found/Content.tsx
@@ -3,17 +3,14 @@ import CtaButton from "../common/CtaButton";
 
 const NotFoundContent = () => {
   return (
-    <div className="w-full h-full font-Inter text-slate-50 flex flex-col lg:flex-row gap-10 justify-center items-start">
-      <h1 className="lg:text-6xl md:text-4xl text-2xl md:leading-relaxed leading-relaxed lg:leading-relaxed font-thin lg:w-3/4 lg:-mt-10 ">
-        Updates in progress. This page will reopen soon.
-        
-        <br className="hidden md:block lg:hidden" />
-         Sorry for the
-        inconvenience.
+    <div className="flex h-full w-full flex-col items-start justify-center gap-10 font-Inter text-slate-50 lg:flex-row">
+      <h1 className="text-2xl font-thin leading-relaxed md:text-4xl md:leading-relaxed lg:-mt-10 lg:w-4/6 lg:text-6xl lg:leading-relaxed">
+        Hey there! Just a heads up, the page is being updated right now. It'll
+        be back up and running soon. Sorry for the wait!
       </h1>
-      <div className="flex flex-col lg:flex-grow w-full lg:w-auto gap-10 justify-start items-center">
+      <div className="flex w-full flex-col items-center justify-start gap-10 lg:w-auto lg:flex-grow">
         <CtaButton className="w-full" />
-        <Link className="text-lg link-with-underline" href="/">
+        <Link className="link-with-underline text-lg" href="/">
           Back to Home
         </Link>
       </div>
